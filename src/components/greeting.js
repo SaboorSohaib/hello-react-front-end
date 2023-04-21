d06828709c1d756068a1a0724a4f72b66eb41470
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-21 13:01:32 +0430

    ADD NEW GREET BUTTON

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -4,18 +4,20 @@ import { showGreeting } from '../redux/greetings/greetings';
 
 function Greeting() {
   const dispatch = useDispatch();
-  const greeting = useSelector((state) => state.greet.greeting);
+  const greeting = useSelector((state) => state.greet);
 
   useEffect(() => {
     dispatch(showGreeting());
   }, [dispatch]);
 
+  const generates = () => {
+    dispatch(showGreeting());
+  };
+
   return (
     <div>
-      <h1>
-        Some One Says
-        {greeting}
-      </h1>
+      <h1>{greeting}</h1>
+      <button onClick={generates} type="button">Generate New Greeting</button>
     </div>
   );
 }
